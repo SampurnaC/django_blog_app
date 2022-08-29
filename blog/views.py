49858774dc9ca20ef05e6c5b5415c5ebbdc49a32
sam <sampurna.chapagainnn@gmail.com>
2022-08-29 09:49:32 +0100

    add home.html and about.html views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Sam',
+        'title': 'Title 1',
+        'content': 'First post content',
+        'date_posted': 'August 10, 2022'
+    },
+    {
+        'author': 'Sam1',
+        'title': 'Title 2',
+        'content': 'First post content 1',
+        'date_posted': 'August 10, 2022'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+    # return HttpResponse('<h1>Blog Home</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
